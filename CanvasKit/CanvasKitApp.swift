487af9c9a1ef800a6199f47372f157396f2abc73
Ridog <4443035+Ridog@users.noreply.github.com>
2025-09-04 19:29:43 -0400

    Initial Commit

---
+++ CanvasKit/CanvasKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasKitApp.swift
+//  CanvasKit
+//
+//  Created by Riley Tench on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CanvasKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CanvasKitTests/CanvasKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasKitTests.swift
+//  CanvasKitTests
+//
+//  Created by Riley Tench on 9/4/25.
+//
+
+import Testing
+@testable import CanvasKit
+
+struct CanvasKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
